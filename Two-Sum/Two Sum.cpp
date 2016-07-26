6e4e6d4c803fe9da3cff46d0ace907a2552d1136
Haiman Bian <bhm2164@163.com>
2016-07-26 15:18:24 +0800

    Update Two Sum.cpp

--- Two-Sum/Two Sum.cpp
+++ Two-Sum/Two Sum.cpp
@@ -9,3 +9,27 @@ Given nums = [2, 7, 11, 15], target = 9,
 Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
 */
+
+
+// v1.0，暴力解决
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+      vector<int> res;
+      int i,j;
+      for(i = 0; i < nums.size() - 1; i++)
+      {
+        for(j = i + 1; j < nums.size(); j++)
+        {
+          if(nums[i] + nums[j] == target)
+          {
+            res.push_back(i);
+            res.push_back(j);
+            break;
+          }
+        }
+      }
+      return res;
+    }
+};
